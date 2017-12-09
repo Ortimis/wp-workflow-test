11379be13043bbc66eb3402139a233a95110e7c8
Ortimis <jakob@drortmann.de>
2017-12-09 13:58:27 +0100

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	wp-content/themes/awps-master/assets/css/style.min.css
    #	wp-content/themes/awps-master/footer.php
    #	wp-content/themes/awps-master/src/scss/base/_containers.scss
    #	wp-content/themes/awps-master/src/scss/components/_footer.scss
    #	wp-content/themes/awps-master/src/scss/pages/_home.scss
    #	wp-content/themes/awps-master/views/content.php

--- wp-content/themes/awps-master/front-page.php
+++ wp-content/themes/awps-master/front-page.php
@@ -32,6 +32,21 @@ get_header(); ?>
 
             </div> <!-- .vide -->
           </section> <!-- .hero -->
+
+					<section class="featured">
+						<div class="container">
+							<div class="row">
+								<div class="col-lg-2">
+
+								</div>
+								<div class="col-lg-8">
+									<iframe width="100%" height="350" src="https://www.youtube.com/embed/tHjdFuHvVtg?start=44&end=55" frameborder="0" gesture="media" allow="encrypted-media" allowfullscreen></iframe>
+								</div>
+							</div>
+						</div>
+					</section>
+
+
           <section class="kacheln" id="kacheln">
             <div class="container">
               <div class="grid">
@@ -74,8 +89,50 @@ get_header(); ?>
             </div>
           </section>
 
+					<section class="fragmente">
+						<div class="container">
+							<div class="row fragment-row">
+								<div class="col-lg-6 video-wrapper">
+									<div class="fb-video" data-href="https://www.facebook.com/resonancefreiburg/videos/1942291532708888/" data-width="600" data-show-text="false"><blockquote cite="https://www.facebook.com/resonancefreiburg/videos/1942291532708888/" class="fb-xfbml-parse-ignore"><a href="https://www.facebook.com/resonancefreiburg/videos/1942291532708888/">Resonanz der Hoffnung | l&#039;Orfeo 2017</a><p>Resonanz der Hoffnung</p>Posted by <a href="https://www.facebook.com/resonancefreiburg/">Resonance</a> on Sonntag, 30. Juli 2017</blockquote></div>
+								</div>
+								<div class="col-lg-6">
+									<p class="shout-out">Musik<br> macht ihn<br> zum Übermenschen.</p>
+								</div>
+							</div>
+							<div class="row fragment-row">
+								<div class="col-lg-6">
+									<p class="shout-out">Ihr seid<br> Orpheus'<br>Waffe!</p>
+								</div>
+								<div class="col-lg-6 video-wrapper">
+									<div class="fb-video" data-href="https://www.facebook.com/resonancefreiburg/videos/1959457167658991/" data-width="600" data-show-text="false"><blockquote cite="https://www.facebook.com/resonancefreiburg/videos/1959457167658991/" class="fb-xfbml-parse-ignore"><a href="https://www.facebook.com/resonancefreiburg/videos/1959457167658991/">Orpheus&#039; Waffe | l&#039;Orfeo 2017</a><p>Schon mal in einem Konzert eingeschlafen?</p>Posted by <a href="https://www.facebook.com/resonancefreiburg/">Resonance</a> on Donnerstag, 7. September 2017</blockquote></div>
+								</div>
+
+							</div>
+							<div class="row fragment-row">
+								<div class="col-lg-6 video-wrapper">
+									<div class="fb-video" data-href="https://www.facebook.com/resonancefreiburg/videos/1955057844765590/" data-width="600" data-show-text="false"><blockquote cite="https://www.facebook.com/resonancefreiburg/videos/1955057844765590/" class="fb-xfbml-parse-ignore"><a href="https://www.facebook.com/resonancefreiburg/videos/1955057844765590/">GRENZGANG | l&#039;Orfeo 2017</a><p>Orpheus überschreitet Grenzen zum Tod, zwischen Menschen, mithilfe seiner Musik. Er wird geliebt.
+Eine Allegorie auf die Macht der Musik?
+
+23./24.09. Christuskirche Freiburg | l&#039;Orfeo - Monteverdi (Szenische Darstellung)</p>Posted by <a href="https://www.facebook.com/resonancefreiburg/">Resonance</a> on Sonntag, 27. August 2017</blockquote></div>
+								</div>
+								<div class="col-lg-6">
+									<p class="shout-out">Es gibt<br> keine Grenzen<br> mehr.</p>
+								</div>
+							</div>
+						</div>
+					</section>
+
 
 			</main><!-- #main -->
 		</div><!-- #primary -->
+
+		<div id="fb-root"></div>
+<script>(function(d, s, id) {
+  var js, fjs = d.getElementsByTagName(s)[0];
+  if (d.getElementById(id)) return;
+  js = d.createElement(s); js.id = id;
+  js.src = 'https://connect.facebook.net/de_DE/sdk.js#xfbml=1&version=v2.11';
+  fjs.parentNode.insertBefore(js, fjs);
+}(document, 'script', 'facebook-jssdk'));</script>
 <?php
 get_footer();
